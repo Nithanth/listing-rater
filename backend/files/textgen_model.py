2014472c70a262422a178d44e6440a5bf18367a6
Nithanth <nithanth.ram@utexas.edu>
2023-02-05 13:32:22 -0600

    Open AI and Cohere script for textgen

---
+++ backend/files/textgen_model.py
@@ -0,0 +1,47 @@
+import os
+import openai
+import cohere
+from secrets import COHERE_KEY, OPENAI_KEY
+
+MODEL = "text-davinci-002"
+
+def generate_property_description_openai(address):
+    
+
+    openai.api_key = OPENAI_KEY
+    response = openai.Completion.create(
+    model=MODEL,
+    prompt=f"Write an Airbnb-like description that describes the property and ammenties really well to customers for this address: {address}",
+    max_tokens=2048,
+    temperature=0.5
+    )
+    # response = requests.post("https://api.openai.com/v1/engines/{}/jobs".format(MODEL),
+    #   headers={
+    #     "Content-Type": "application/json",
+    #     "Authorization": f"Bearer {API_KEY}"
+    #   },
+    #   json={
+    #     "prompt": prompt,
+    #     "max_tokens": 2048,
+    #     "temperature": 0.5,
+    #   }
+    # )
+
+    return response["choices"][0]["text"]
+
+def generate_property_description_cohere(address):
+    co = cohere.Client(COHERE_KEY)
+    co_prompt=f"Write an Airbnb-like description that describes the property and ammenties really well to customers for this address: {address}"
+    response = co.generate(  
+        model='command-xlarge-nightly',  
+        prompt = co_prompt,  
+        max_tokens=400,  
+        temperature=0.5,  
+        stop_sequences=["--"])
+    return response.generations[0].text
+
+
+address = input("Enter an address: ")
+property_description = generate_property_description_cohere(address)
+print("Description of the property:")
+print(property_description)
